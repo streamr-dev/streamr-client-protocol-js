3993cbeb3c771c1800006c55c31636f605cc5cd1
mthambipillai <melchior.thambipillai@streamr.com>
2018-12-13 16:33:30 +0100

    fill index.js with files to export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,159 @@
+import BroadcastMessage from './protocol/control_layer/broadcast_message/BroadcastMessage'
+import BroadcastMessageFactory from './protocol/control_layer/broadcast_message/BroadcastMessageFactory'
+import BroadcastMessageV0 from './protocol/control_layer/broadcast_message/BroadcastMessageV0'
+import BroadcastMessageV1 from './protocol/control_layer/broadcast_message/BroadcastMessageV1'
 
+import ErrorPayload from './protocol/control_layer/error_response/ErrorPayload'
+import ErrorResponse from './protocol/control_layer/error_response/ErrorResponse'
+import ErrorResponseFactory from './protocol/control_layer/error_response/ErrorResponseFactory'
+import ErrorResponseV0 from './protocol/control_layer/error_response/ErrorResponseV0'
+import ErrorResponseV1 from './protocol/control_layer/error_response/ErrorResponseV1'
+
+import PublishRequest from './protocol/control_layer/publish_request/PublishRequest'
+import PublishRequestFactory from './protocol/control_layer/publish_request/PublishRequestFactory'
+import PublishRequestV0 from './protocol/control_layer/publish_request/PublishRequestV0'
+import PublishRequestV1 from './protocol/control_layer/publish_request/PublishRequestV1'
+
+import ResendFromRequestFactory from './protocol/control_layer/resend_request/ResendFromRequestFactory'
+import ResendFromRequestV1 from './protocol/control_layer/resend_request/ResendFromRequestV1'
+import ResendLastRequestFactory from './protocol/control_layer/resend_request/ResendLastRequestFactory'
+import ResendLastRequestV1 from './protocol/control_layer/resend_request/ResendLastRequestV1'
+import ResendRangeRequestFactory from './protocol/control_layer/resend_request/ResendRangeRequestFactory'
+import ResendRangeRequestV1 from './protocol/control_layer/resend_request/ResendRangeRequestV1'
+import ResendRequestV0 from './protocol/control_layer/resend_request/ResendRequestV0'
+
+import ResendResponseNoResend from './protocol/control_layer/resend_response_no_resend/ResendResponseNoResend'
+import ResendResponseNoResendFactory from './protocol/control_layer/resend_response_no_resend/ResendResponseNoResendFactory'
+import ResendResponseNoResendV0 from './protocol/control_layer/resend_response_no_resend/ResendResponseNoResendV0'
+import ResendResponseNoResendV1 from './protocol/control_layer/resend_response_no_resend/ResendResponseNoResendV1'
+
+import ResendResponseResending from './protocol/control_layer/resend_response_resending/ResendResponseResending'
+import ResendResponseResendingFactory from './protocol/control_layer/resend_response_resending/ResendResponseResendingFactory'
+import ResendResponseResendingV0 from './protocol/control_layer/resend_response_resending/ResendResponseResendingV0'
+import ResendResponseResendingV1 from './protocol/control_layer/resend_response_resending/ResendResponseResendingV1'
+
+import ResendResponseResent from './protocol/control_layer/resend_response_resent/ResendResponseResent'
+import ResendResponseResentFactory from './protocol/control_layer/resend_response_resent/ResendResponseResentFactory'
+import ResendResponseResentV0 from './protocol/control_layer/resend_response_resent/ResendResponseResentV0'
+import ResendResponseResentV1 from './protocol/control_layer/resend_response_resent/ResendResponseResentV1'
+
+import SubscribeRequest from './protocol/control_layer/subscribe_request/SubscribeRequest'
+import SubscribeRequestFactory from './protocol/control_layer/subscribe_request/SubscribeRequestFactory'
+import SubscribeRequestV0 from './protocol/control_layer/subscribe_request/SubscribeRequestV0'
+import SubscribeRequestV1 from './protocol/control_layer/subscribe_request/SubscribeRequestV1'
+
+import SubscribeResponse from './protocol/control_layer/subscribe_response/SubscribeResponse'
+import SubscribeResponseFactory from './protocol/control_layer/subscribe_response/SubscribeResponseFactory'
+import SubscribeResponseV0 from './protocol/control_layer/subscribe_response/SubscribeResponseV0'
+import SubscribeResponseV1 from './protocol/control_layer/subscribe_response/SubscribeResponseV1'
+
+import UnicastMessage from './protocol/control_layer/unicast_message/UnicastMessage'
+import UnicastMessageFactory from './protocol/control_layer/unicast_message/UnicastMessageFactory'
+import UnicastMessageV0 from './protocol/control_layer/unicast_message/UnicastMessageV0'
+import UnicastMessageV1 from './protocol/control_layer/unicast_message/UnicastMessageV1'
+
+import UnsubscribeRequest from './protocol/control_layer/unsubscribe_request/UnsubscribeRequest'
+import UnsubscribeRequestFactory from './protocol/control_layer/unsubscribe_request/UnsubscribeRequestFactory'
+import UnsubscribeRequestV0 from './protocol/control_layer/unsubscribe_request/UnsubscribeRequestV0'
+import UnsubscribeRequestV1 from './protocol/control_layer/unsubscribe_request/UnsubscribeRequestV1'
+
+import UnsubscribeResponse from './protocol/control_layer/unsubscribe_response/UnsubscribeResponse'
+import UnsubscribeResponseFactory from './protocol/control_layer/unsubscribe_response/UnsubscribeResponseFactory'
+import UnsubscribeResponseV0 from './protocol/control_layer/unsubscribe_response/UnsubscribeResponseV0'
+import UnsubscribeResponseV1 from './protocol/control_layer/unsubscribe_response/UnsubscribeResponseV1'
+
+import ControlMessage from './protocol/control_layer/ControlMessage'
+import ControlMessageFactory from './protocol/control_layer/ControlMessageFactory'
+import ControlMessageV0Factory from './protocol/control_layer/ControlMessageV0Factory'
+import ResendResponsePayload from './protocol/control_layer/ResendResponsePayload'
+import StreamAndPartition from './protocol/control_layer/StreamAndPartition'
+
+import MessageID from './protocol/message_layer/MessageID'
+import MessageRef from './protocol/message_layer/MessageRef'
+import StreamMessage from './protocol/message_layer/StreamMessage'
+import StreamMessageFactory from './protocol/message_layer/StreamMessageFactory'
+import StreamMessageV28 from './protocol/message_layer/StreamMessageV28'
+import StreamMessageV29 from './protocol/message_layer/StreamMessageV29'
+import StreamMessageV30 from './protocol/message_layer/StreamMessageV30'
+
+import InvalidJsonError from './errors/InvalidJsonError'
+import UnsupportedVersionError from './errors/UnsupportedVersionError'
+import TimestampUtil from './utils/TimestampUtil'
+
+module.exports = {
+    ControlLayer: {
+        BroadcastMessage,
+        BroadcastMessageFactory,
+        BroadcastMessageV0,
+        BroadcastMessageV1,
+        ErrorPayload,
+        ErrorResponse,
+        ErrorResponseFactory,
+        ErrorResponseV0,
+        ErrorResponseV1,
+        PublishRequest,
+        PublishRequestFactory,
+        PublishRequestV0,
+        PublishRequestV1,
+        ResendFromRequestFactory,
+        ResendFromRequestV1,
+        ResendLastRequestFactory,
+        ResendLastRequestV1,
+        ResendRangeRequestFactory,
+        ResendRangeRequestV1,
+        ResendRequestV0,
+        ResendResponseNoResend,
+        ResendResponseNoResendFactory,
+        ResendResponseNoResendV0,
+        ResendResponseNoResendV1,
+        ResendResponseResending,
+        ResendResponseResendingFactory,
+        ResendResponseResendingV0,
+        ResendResponseResendingV1,
+        ResendResponseResent,
+        ResendResponseResentFactory,
+        ResendResponseResentV0,
+        ResendResponseResentV1,
+        SubscribeRequest,
+        SubscribeRequestFactory,
+        SubscribeRequestV0,
+        SubscribeRequestV1,
+        SubscribeResponse,
+        SubscribeResponseFactory,
+        SubscribeResponseV0,
+        SubscribeResponseV1,
+        UnicastMessage,
+        UnicastMessageFactory,
+        UnicastMessageV0,
+        UnicastMessageV1,
+        UnsubscribeRequest,
+        UnsubscribeRequestFactory,
+        UnsubscribeRequestV0,
+        UnsubscribeRequestV1,
+        UnsubscribeResponse,
+        UnsubscribeResponseFactory,
+        UnsubscribeResponseV0,
+        UnsubscribeResponseV1,
+        ControlMessage,
+        ControlMessageFactory,
+        ControlMessageV0Factory,
+        ResendResponsePayload,
+        StreamAndPartition,
+    },
+    MessageLayer: {
+        MessageID,
+        MessageRef,
+        StreamMessage,
+        StreamMessageFactory,
+        StreamMessageV28,
+        StreamMessageV29,
+        StreamMessageV30,
+    },
+    Errors: {
+        InvalidJsonError,
+        UnsupportedVersionError,
+    },
+    Utils: {
+        TimestampUtil,
+    },
+}
--- src/protocol/control_layer/publish_request/PublishRequestV0.js
+++ src/protocol/control_layer/publish_request/PublishRequestV0.js
@@ -4,8 +4,6 @@ import StreamMessageV30 from '../../message_layer/StreamMessageV30'
 import StreamMessage from '../../message_layer/StreamMessage'
 import PublishRequest from './PublishRequest'
 
-const murmur = require('murmurhash-native').murmurHash
-
 const TYPE = 'publish'
 const VERSION = 0
 
@@ -29,29 +27,11 @@ class PublishRequestV0 extends PublishRequest {
         this.signature = signature
     }
 
-    getStreamMessage(partitionCount) {
-        const streamPartition = this.getStreamPartition(partitionCount)
+    getStreamMessage(streamPartition) {
         return new StreamMessageV30(
             [this.streamId, streamPartition, this.timestamp, 0, this.publisherAddress], [null, null],
             StreamMessage.CONTENT_TYPES.JSON, this.content, this.signatureType, this.signature,
         )
-    }
-
-    getStreamPartition(partitionCount) {
-        if (!partitionCount) {
-            throw new Error('partitionCount is falsey!')
-        } else if (partitionCount === 1) {
-            // Fast common case
-            return 0
-        } else if (this.partitionKey) {
-            const bytes = Buffer.from(this.partitionKey, 'utf8')
-            const resultBytes = murmur(bytes, 0, 'buffer')
-            const intHash = resultBytes.readInt32LE()
-            return Math.abs(intHash) % partitionCount
-        } else {
-            // Fallback to random partition if no key
-            return Math.floor(Math.random() * partitionCount)
-        }
     }
 
     getSerializedContent() {
