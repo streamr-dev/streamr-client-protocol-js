ae7db71fc2188000b0984ae7ea322769f5fddc83
Miroslav Pokrovskii <miroslavpokrovskiy@gmail.com>
2019-09-25 15:47:06 +0300

    Update .travis, add test for ControlLayer.ControlMessage.deserialize

--- test/unit/protocol/message_layer/StreamMessageFactory.test.js
+++ test/unit/protocol/message_layer/StreamMessageFactory.test.js
@@ -1,11 +1,16 @@
 import assert from 'assert'
 import StreamMessageFactory from '../../../../src/protocol/message_layer/StreamMessageFactory'
+import PublishRequestV1 from '../../../../src/protocol/control_layer/publish_request/PublishRequestV1'
+import BroadcastMessageV1 from '../../../../src/protocol/control_layer/broadcast_message/BroadcastMessageV1'
 import StreamMessage from '../../../../src/protocol/message_layer/StreamMessage'
 import StreamMessageV28 from '../../../../src/protocol/message_layer/StreamMessageV28'
 import StreamMessageV29 from '../../../../src/protocol/message_layer/StreamMessageV29'
 import StreamMessageV30 from '../../../../src/protocol/message_layer/StreamMessageV30'
+import StreamMessageV31 from '../../../../src/protocol/message_layer/StreamMessageV31'
 import InvalidJsonError from '../../../../src/errors/InvalidJsonError'
 import UnsupportedVersionError from '../../../../src/errors/UnsupportedVersionError'
+// import { ControlLayer } from 'streamr-client-protocol'
+import { ControlLayer } from '../../../../src/index'
 
 describe('StreamMessageFactory', () => {
     describe('deserialize', () => {
@@ -63,6 +68,37 @@ describe('StreamMessageFactory', () => {
             assert.deepEqual(result.parsedContent, {
                 valid: 'json',
             })
+        })
+        it('should correctly deserialize StreamMessageV29 with parsedContent = false and create correct BroadcastMessage', () => {
+            const json = '[1,8,[31,["kxeE-gyxS8CkuWYlfBKMVg",0,1567671580680,0,' +
+                '"0x8a9b2ca74d8c1c095d34de3f3cdd7462a5c9c9f4b84d11270a0ad885958bb963",' +
+                '"7kcxFuyOs4ozeAcVfzJF"],[1567671579675,0],27,0,"{\\"random\\": 0.8314497807870005}",0,null],' +
+                '"kuC8Ilzt2NURdpKxuYN2JBLkPQBJ0vN7NGIx5ohA7ZJafyh29I07fZR57Jq4fUBo"]'
+
+            const PARSE_CONTENT = false
+
+            // PARSE_CONTENT = true and any version - works ok
+            // PARSE_CONTENT = false and the same version = 31 - works ok
+            // PARSE_CONTENT = false and the different version = 30 - works no
+
+            const request = ControlLayer.ControlMessage.deserialize(json, PARSE_CONTENT)
+            assert(request instanceof PublishRequestV1)
+            const streamMessage = request.getStreamMessage()
+            assert(streamMessage instanceof StreamMessageV31)
+            // console.log(streamMessage)
+            // console.log(request.serialize(31))
+
+            // const broadcastMessage = ControlLayer.BroadcastMessage.create(streamMessage)
+            // assert(broadcastMessage instanceof BroadcastMessageV1)
+
+            const newRequest = ControlLayer.ControlMessage.deserialize(request.serialize(30), PARSE_CONTENT)
+            assert(newRequest instanceof PublishRequestV1)
+            // console.log(newRequest.serialize(31))
+            const newStreamMessage = request.getStreamMessage()
+            assert(newStreamMessage instanceof StreamMessageV31)
+
+            console.log(newStreamMessage)
+            // console.log(newRequest.serialize(31))
         })
     })
 })
