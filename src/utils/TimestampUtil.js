a1e52d9583d55c2fed9261752b5d61d1eed83f88
mirpo <miroslavpokrovskiy@gmail.com>
2020-05-05 13:40:53 +0300

    Refactor

--- src/utils/TimestampUtil.js
+++ src/utils/TimestampUtil.js
@@ -2,16 +2,12 @@ export function parse(millisOrString) {
     if (typeof millisOrString === 'number') {
         return millisOrString
     }
-    if (typeof millisOrString === 'string') {
-        // Try if this string represents a number
-        const timestamp = Number(millisOrString) || Date.parse(millisOrString)
-        if (Number.isNaN(timestamp)) {
-            throw new Error(`Invalid timestamp: ${millisOrString}`)
-        } else {
-            return timestamp
-        }
-    } else {
-        throw new Error(`Invalid timestamp: ${millisOrString}`)
-    }
-}
 
+    const timestamp = Number(millisOrString) || Date.parse(millisOrString)
+
+    if (Number.isInteger(timestamp)) {
+        return timestamp
+    }
+
+    throw new Error(`Invalid timestamp: ${millisOrString}`)
+}
